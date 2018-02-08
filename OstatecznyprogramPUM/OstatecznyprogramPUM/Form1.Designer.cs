efe76c0cf594c37fe2209d8e01b7503188d13185
KatarzynaWidawka <34284333+KatarzynaWidawka@users.noreply.github.com>
2018-02-08 07:58:09 +0100

    Program WinForms

---
+++ OstatecznyprogramPUM/OstatecznyprogramPUM/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace OstatecznyprogramPUM
+{
+    partial class CiagFibonacciego
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(283, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 99);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 135);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(423, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(212, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Wynik:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CiagFibonacciego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::OstatecznyprogramPUM.Properties.Resources.galaxy;
+            this.ClientSize = new System.Drawing.Size(456, 270);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CiagFibonacciego";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CiagFibonacciego";
+            this.Load += new System.EventHandler(this.CiagFibonacciego_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OstatecznyprogramPUM/OstatecznyprogramPUM/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OstatecznyprogramPUM
+{
+    public partial class CiagFibonacciego : Form
+    {
+        public CiagFibonacciego()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        volatile int n;
+        volatile int wynik;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            if(textBox1.Text!="")
+            {
+                n = int.Parse(textBox1.Text);
+                progressBar1.Maximum = n - 1;
+                progressBar1.Step = 1;
+                progressBar1.Value = 1;
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e) {  }
+        
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            int f1 = 0; //wartość pierwszego elementu
+            int f2 = 1; //wartość drugiego elementu
+            for (int i=0;i<n-1;i++)
+            {
+               
+                wynik= f1 + f2;
+                f1 = f2;
+                f2 = wynik;
+
+
+                System.Threading.Thread.Sleep(200);
+                progressBar1.Invoke(new Action(() => progressBar1.PerformStep()));
+                textBox2.Invoke(new Action(() => textBox2.Text = wynik.ToString()));
+            }
+            
+            
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        { textBox2.Text = "Wynik:" + wynik; }
+        private void CiagFibonacciego_Load(object sender, EventArgs e) { }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
